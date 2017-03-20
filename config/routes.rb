cad3e28f79f9d616814cb3dbcf6cdd8a72c8a8af
Alain Hélaïli <helaili@github.com>
2017-03-20 17:43:09 +0100

    basic wrapper on flipper

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,12 @@
 require 'flipper'
-
 require "flipper-api"
 require "flipper-ui"
-require "flipper/adapters/pstore"
-
-Flipper.register(:admins) { |actor|
-  actor.respond_to?(:admin?) && actor.admin?
-}
-
-Flipper.register(:early_access) { |actor|
-  actor.respond_to?(:early?) && actor.early?
-}
-
-# Setup logging of flipper calls.
-$logger = Logger.new(STDOUT)
-require "active_support/notifications"
-require "flipper/instrumentation/log_subscriber"
-Flipper::Instrumentation::LogSubscriber.logger = $logger
-
-adapter = Flipper::Adapters::PStore.new
-flipper = Flipper.new(adapter, instrumenter: ActiveSupport::Notifications)
 
 Soloflipper::Application.routes.draw do
-  mount Flipper::Api.app(flipper) => '/flipper/api'
-  mount Flipper::UI.app(flipper) => '/flipper'
+  get 'flipper_wrapper/:feature/isEnabledFor/:user', to: 'flipper_wrapper#show'
+
+  get 'welcome/index'
+
+  mount Flipper::Api.app(Rails.application.config.flipper) => '/flipper/api'
+  mount Flipper::UI.app(Rails.application.config.flipper) => '/flipper'
 end
